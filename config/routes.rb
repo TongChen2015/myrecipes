46f5faad89e5526c597922c331a784b411b10af7
Tong Chen <timothy.ct@gmail.com>
2018-02-22 06:13:47 +0000

    Create new and add partials

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'pages#home'
+  
   get '/home', to: 'pages#home'
+  
+  resources :recipes
   
 end
