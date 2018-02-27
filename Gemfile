9d481319fc8562f41964982e8cb15f4f83534f6b
Tong Chen <timothy.ct@gmail.com>
2018-02-27 06:35:25 +0000

    Add chef authentication, new, edit for new chefs

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'carrierwave', '1.2.2'
 gem 'mini_magick', '4.8.0'
 gem 'will_paginate', '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
+gem 'bcrypt', '~> 3.1.11'
 
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226054639) do
+ActiveRecord::Schema.define(version: 20180227055504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180226054639) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "likes", force: :cascade do |t|
