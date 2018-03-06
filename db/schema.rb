534bab5afede0abff9aaec2a2ef01b1e23f00ab3
Tong Chen <timothy.ct@gmail.com>
2018-03-06 05:59:24 +0000

    build many to many associations style and ingredient to recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304063521) do
+ActiveRecord::Schema.define(version: 20180306053603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20180304063521) do
     t.integer "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "ingredient_id"
+    t.integer "recipe_id"
+  end
+
+  create_table "recipe_styles", force: :cascade do |t|
+    t.integer "style_id"
+    t.integer "recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
